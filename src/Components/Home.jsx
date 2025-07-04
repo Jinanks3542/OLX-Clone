611eea0923e07e1c688fae63ad62a684bc2926d8
Jinanks3542 <jinanks66@gmail.com>
2025-07-04 10:22:59 +0530

    Initial commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar/Navbar'
+import Login from './Modal/Login'
+function Home() {
+
+    const [openModal,setModal] = useState(false)
+    const toggleModal = ()=> {setModal(!openModal)}
+  return (
+    <div>
+        <Navbar  toggleModal={toggleModal}/>
+        <Login toggleModal= {toggleModal} status ={openModal} />
+    </div>
+  )
+}
+
+export default Home
